89088f052c5b1f4bb329841324fc5dccb67d039e
JohnCoene <jcoenep@gmail.com>
2020-11-04 15:09:24 +0100

    init mod

---
+++ ergo/go.mod
@@ -0,0 +1,3 @@
+module github.com/JohnCoene/go-ergo/ergo
+
+go 1.15
--- ergo/play_test.go
+++ ergo/play_test.go
@@ -1,12 +1,10 @@
 package ergo
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestPlay(t *testing.T) {
 	p := Play(100, 100, 100)
-	avg := p.EnsembleAvg()
-	fmt.Println(avg)
+	p.EnsembleAvg()
 }
